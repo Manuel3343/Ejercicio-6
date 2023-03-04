076f164af9e2cb709b8480737a18eca28377af20
Manuel3343 <126327609+Manuel3343@users.noreply.github.com>
2023-03-03 21:47:34 -0500

    Add files via upload

---
+++ mavenproject5/src/main/java/com/mycompany/mavenproject5/Mavenproject5.java
@@ -0,0 +1,26 @@
+package com.mycompany.mavenproject5;
+ import java.util.Scanner;
+
+public class Mavenproject5{
+    public static void main(String[] args) {
+        
+        //Inicio;
+        double SUMA;
+        double  X;
+        SUMA = 0;
+        X = 20;
+        System.out.println("El valor de la SUMA es: " + SUMA);
+        SUMA = SUMA +X;
+        System.out.println("El valor de la X es: " + X);
+        System.out.println("El valor de la SUMA es: " + SUMA);
+        double Y;
+        Y = 40;
+        X = X + (Y*Y);
+        System.out.println("El valor de la Y es: " + Y);
+        System.out.println("El valor de la X es: " + X);
+        SUMA = SUMA + X/Y;
+        System.out.println("El valor de la SUMA es: " + SUMA);
+        //FIN_INICIO
+    }
+         
+}
